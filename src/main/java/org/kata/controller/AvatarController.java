c3ebb3b3c054b82eda04b8b33dfe391ef54f5ac8
defeatedSanity <rome.kuznets@gmail.com>
2023-10-31 14:51:42 +0300

    добавляет swagger-аннотации в контроллер

--- src/main/java/org/kata/controller/AvatarController.java
+++ src/main/java/org/kata/controller/AvatarController.java
@@ -11,6 +11,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,35 +22,52 @@ import java.util.List;
 public class AvatarController {
 
     private final AvatarService avatarService;
-
+    @Operation(summary = "Создать новый Avatar", description = "Сохраняет и возвращает DTO нового аватара")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Avatar успешно создан"),
+            @ApiResponse(responseCode = "400", description = "Неверный запрос")
+    })
     @PostMapping("/createAvatar")
     public ResponseEntity<AvatarDto> uploadAvatar(@RequestParam("icp") String icp, @RequestParam("file") MultipartFile file) {
         return new ResponseEntity<>(
                 avatarService.createAvatarDto(icp, file), HttpStatus.OK);
     }
-
+    @Operation(summary = "Получить Avatar по icp",
+            description= "Возвращает DTO Avatar по ICP")
     @GetMapping("/getAvatar")
-    public ResponseEntity<AvatarDto> getAvatar(@RequestParam("icp") String icp) {
+    public ResponseEntity<AvatarDto> getAvatar(@Parameter(description = "ICP для получения") @RequestParam("icp") String icp) {
         return avatarService.getAvatarDto(icp);
     }
-
+    @Operation(summary = "Получить список Avatar по icp",
+            description= "Возвращает список DTO Avatar по ICP")
     @GetMapping("/getAllAvatars")
-    public ResponseEntity<List<AvatarDto>> getAllAvatars(@RequestParam("icp") String icp) {
+    public ResponseEntity<List<AvatarDto>> getAllAvatars(@Parameter(description = "ICP для получения List<Avatar>") @RequestParam("icp") String icp) {
         return avatarService.getAllAvatarsDto(icp);
     }
-
+    @Operation(summary = "Запрос на удаление аватаров по icp и списку флагов",
+            description = "Запрос на удаление одного или нескольких Avatar по icp и списку boolean (галочки) в соответствии со списком getAllAvatars(String icp)"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Avatar успешно удалён"),
+            @ApiResponse(responseCode = "400", description = "Неверный запрос")
+    })
     @DeleteMapping("/deleteAvatars")
-    public ResponseEntity<HttpStatus> deleteAvatars(@RequestParam("icp") String icp,
-                                                    @RequestParam("flags") List<Boolean> flags) {
+    public ResponseEntity<HttpStatus> deleteAvatars(@Parameter(description = "ICP для удаления") @RequestParam("icp") String icp,
+                                                    @Parameter(description = "Флаги для удаления") @RequestParam("flags") List<Boolean> flags) {
         avatarService.deleteAvatars(icp, flags);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
+    @Operation(summary = "Обновляет активный аватар",
+            description= "Обновляет аватар по icp и файлу-изображению")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Avatar успешно обновлён"),
+            @ApiResponse(responseCode = "400", description = "Неверный запрос")
+    })
     @PatchMapping("/setActiveAvatar")
-    public ResponseEntity<AvatarDto> setActiveAvatar(@RequestParam("icp") String icp,
-                                                     @RequestParam("file") MultipartFile file) {
+    public ResponseEntity<AvatarDto> setActiveAvatar(@Parameter(description = "ICP для обновления") @RequestParam("icp") String icp,
+                                                     @Parameter(description = "Изображение для обновления") @RequestParam("file") MultipartFile file) {
         return new ResponseEntity<>(
-                avatarService.createAvatarDto(icp, file), HttpStatus.OK);
+                avatarService.createAvatarDto(icp, file), HttpStatus.ACCEPTED);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
