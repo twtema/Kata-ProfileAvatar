72fa509f7732331645951b57609e11ff8371fd32
Roman Kuznetsov <136959697+defeatedSanity@users.noreply.github.com>
2023-11-06 11:02:25 +0300

    [Task-13] - Добавляет доп. crud методы, класс ImageProcessor для сжатия фото (#2)
    * добавляет доп. crud методы, класс ImageProcessor для сжатия фото
    
    * добавляет swagger-аннотации в контроллер
    
    * swagger к avatar контроллеру, проверка фото на целостность
    
    * change return types in service, more reliable getting List in WebClient

--- src/main/java/org/kata/dto/AvatarDto.java
+++ src/main/java/org/kata/dto/AvatarDto.java
@@ -1,6 +1,7 @@
 package org.kata.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
@@ -10,9 +11,12 @@ import lombok.extern.jackson.Jacksonized;
 @Jacksonized
 public class AvatarDto {
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Schema(description = "ICP", example = "1234567890")
     private String icp;
 
+    @Schema(description = "Имя файла", example = "avatar.png")
     private String filename;
 
+    @Schema(description = "Данные изображения, максимум 5МБ")
     private byte[] imageData;
 }
